6392b1baa37cb096dab91afa512dbe963a042184
Zachary Baca <cubsfan.46350@gmail.com>
2019-01-06 14:54:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const paragraph = React.createElement('p', {}, 'Who is learning React');
 const javascript = React.createElement('li', {}, 'JavaScript');
 const react = React.createElement('li',{}, 'React');
 const movies = React.createElement('li',{},'Movies');
-const iceCream = React.createElement('li',{},'Ice Cream');
-const list = React.createElement('ul', {},javascript,react,movies,iceCream);
+const iceCream = React.createElement('li',{},'Ice cream');
+const list = React.createElement('ul', {className: 'my-interests'},javascript,react,movies,iceCream);
 const meInReact = React.createElement('div', {className : 'me'},title,paragraph,list );
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
